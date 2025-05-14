b3be8d993f1c014ac2a5ab73e2495e68e01a7615
Dmitrii Deruzhinskii <d.deruzh@gmail.com>
2025-05-14 15:18:15 +0300

    feat: common header for library
    It is more comfortable to use small header-only libraries when they provide one header to include.

---
+++ include/mockstream/mockstream.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Entities
+#include <mockstream/common.h>
+#include <mockstream/stream_point.h>
+
+// Models
+#include <mockstream/model.h>
+#include <mockstream/models/fixed.h>
+#include <mockstream/models/identity.h>
